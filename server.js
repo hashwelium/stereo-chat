1c9faad70291429ce7c981424b5e9b0d97596d28
hashwelium <taisei52213@gmail.com>
2026-02-07 20:11:56 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3000 });
-
-let clients = [];
-
-wss.on('connection', ws => {
-  clients.push(ws);
-  ws.on('message', msg => {
-    clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(msg.toString());
-      }
-    });
-  });
-});
-
-console.log("Signaling server running on port 3000");
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: "*",
