291860da2fd1d8b56705dc2f0d728720badc5cbb
hashwelium <taisei52213@gmail.com>
2026-02-07 20:09:12 +0900

    Update server.js

--- server.js
+++ server.js
@@ -19,4 +19,31 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("offer", (data) => socket.broadcast.emit("offer", data));
+  socket.on("answer", (data) => socket.broadcast.emit("answer", data));
+  socket.on("ice-candidate", (data) => socket.broadcast.emit("ice-candidate", data));
+
+  socket.on("disconnect", () => console.log("User disconnected"));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
 
